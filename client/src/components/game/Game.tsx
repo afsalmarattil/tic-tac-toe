15a0b5a14d018cef1a836a050c3de6edc3afddb6
afsalmarattil <afsal.marattil@robok.ai>
2024-05-17 08:25:10 +0100

    feat(ui): added gird size to dynamically change size

--- client/src/components/game/Game.tsx
+++ client/src/components/game/Game.tsx
@@ -2,13 +2,15 @@ import React, { Dispatch, SetStateAction } from "react";
 import { Board } from "./Board";
 import { Controls } from "./Controls";
 import { GameOver } from "./GameOver";
+import { GridSizeInput } from "./GridSizeInput";
 
 export const Game: React.FC = () => {
   return (
     <div className="flex flex-col items-center lg:justify-center lg:items-start mt-10">
       <div className="flex flex-col items-center">
         <Board />
-        <div className="w-full flex justify-end mt-4">
+        <div className="w-full flex justify-between mt-4">
+          <GridSizeInput />
           <Controls />
         </div>
       </div>
